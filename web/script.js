c04ce7728a768004c1817d9b7123b332bc1e49ff
theodorejohn <emailtheodore@gmail.com>
2022-03-05 01:31:03 +0530

    home page transition

--- web/script.js
+++ web/script.js
@@ -1,3 +1,5 @@
+/*scroll bar*/ 
+
 let progress =document.getElementById('progressBar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function(){
@@ -5,3 +7,10 @@ window.onscroll = function(){
     totalHeight) * 100 ;
     progress.style.height = progressHeight+"%";
 }
+
+
+AOS.init({
+    offset: 400, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000 // values from 0 to 3000, with step 50ms
+  });
