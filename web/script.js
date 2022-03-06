b404587b977f9fc7c3aa39e5e089b9bab6733fa1
theodorejohn <emailtheodore@gmail.com>
2022-03-06 06:17:16 +0530

    search bar home page

--- web/script.js
+++ web/script.js
@@ -1,24 +1,24 @@
-/*scroll bar*/ 
+/*scroll bar*/
 
-let progress =document.getElementById('progressBar');
+let progress = document.getElementById('progressBar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / 
-    totalHeight) * 100 ;
-    progress.style.height = progressHeight+"%";
+window.onscroll = function () {
+    let progressHeight = (window.pageYOffset /
+        totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
 }
 
 /* TRANSITION */
 
 AOS.init({
-    offset: 400, // offset (in px) from the original trigger point
+    offset: 100, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000 // values from 0 to 3000, with step 50ms
-  });
+});
 
-  /* QUANTITY COUNTER */
+/* QUANTITY COUNTER */
 
-  let incrementButton = document.getElementsByClassName('inc');
+let incrementButton = document.getElementsByClassName('inc');
 let decrementButton = document.getElementsByClassName('dec');
 
 //INCREMENT
@@ -46,3 +46,15 @@ for (let i = 0; i < decrementButton.length; i++) {
         }
     })
 }
+
+
+
+/* SEARCH BAR */
+
+const icon = document.querySelector('.icon');
+const search = document.querySelector('.search');
+icon.onclick = function(){
+    search.classList.toggle('active')
+}
+
+
